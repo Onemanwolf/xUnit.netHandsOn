61d72918196bf5427ba4a6e2b25e639e4f0ae021
Tim Oleson <timothy.oleson@gmail.com>
2020-08-21 12:17:48 -0400

    Add Some Property Mocks

--- GameEngine.Test/PlayerCharacterShould.cs
+++ GameEngine.Test/PlayerCharacterShould.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using FluentAssertions.Events;
+using Moq;
 using System;
 using System.ComponentModel;
 using Xunit;
@@ -9,85 +10,111 @@ namespace GameEngine.Test
     [Trait("Catogory", "PlayerCharacter")]
     public class PlayerCharacterShould
     {
-        private PlayerCharacter _sut;
-
+        private IPlayerCharacter _sut;
+        private Mock<IPlayerCharacter> mock;
+      
         public PlayerCharacterShould()
         {
             _sut = new PlayerCharacter();
+            mock = new Mock<IPlayerCharacter>();
 
         }
 
         [Fact]
         public void BeInexperiencedWhenNew()
         {
-
-
-            Assert.True(_sut.IsNoob);
-            _sut.IsNoob.Should().BeTrue();
+            //Assert.True(_sut.IsNoob);
+           // _sut.IsNoob.Should().BeTrue();
         }
+
+
         [Fact]
         public void CheckFullName()
         {
-
-            _sut.FirstName = "Tim";
-            _sut.LastName = "Oleson";
+           
+            mock.Setup(pc => pc.FirstName).Returns("Tim");
+            mock.Setup(pc => pc.LastName).Returns("Oleson");
+            mock.Setup(pc => pc.FullName).Returns(string.Concat(mock.Object.FirstName, " ", mock.Object.LastName));
+            //_sut.FirstName = "Tim";
+            //_sut.LastName = "Oleson";
 
             //Assert.Equal("Tim Oleson", _sut.FullName);
-            _sut.FullName.Should().Be("Tim Oleson");
+            mock.Object.FullName.Should().Be("Tim Oleson");
         }
 
         [Fact]
         public void CheckFullNameStartsWith()
         {
-            _sut.FirstName = "Anna";
-            _sut.LastName = "Oleson";
-
+           // _sut.FirstName = "Anna";
+           // _sut.LastName = "Oleson";
+            mock.Setup(pc => pc.FirstName).Returns("Anna");
+            mock.Setup(pc => pc.LastName).Returns("Oleson");
+            mock.Setup(pc => pc.FullName)
+                .Returns(string.Concat(mock.Object.FirstName, " ", mock.Object.LastName));
             //Assert.StartsWith("Anna", _sut.FirstName);
-            _sut.FullName.Should().StartWith("Anna");
+            mock.Object.FullName.Should().StartWith("Anna");
 
         }
         [Fact]
         public void CheckFullNameEndsWith()
         {
-            _sut.FirstName = "Marcela";
-            _sut.LastName = "Oleson";
+           // _sut.FirstName = "Marcela";
+            //_sut.LastName = "Oleson";
+
+            mock.Setup(pc => pc.FirstName).Returns("Anna");
+            mock.Setup(pc => pc.LastName).Returns("Oleson");
+            mock.Setup(pc => pc.FullName).Returns(string.Concat(mock.Object.FirstName, " ", mock.Object.LastName));
+
            // Assert.EndsWith("Oleson", _sut.FullName);
-            _sut.FullName.Should().EndWith("Oleson");
+            mock.Object.FullName.Should().EndWith("Oleson");
         }
 
         [Fact]
         public void CheckLastNameEqualsNotCaseSensative()
         {
-            _sut.FirstName = "Sam";
-            _sut.LastName = "Daves";
-           // Assert.Equal("DAVES", _sut.LastName, ignoreCase: true);
-            _sut.LastName.Should().BeEquivalentTo("DAVES");
+            // _sut.FirstName = "Sam";
+            mock.Setup(pc => pc.LastName).Returns("Daves");
+            //_sut.LastName = "Daves";
+
+            // Assert.Equal("DAVES", _sut.LastName, ignoreCase: true);
+            mock.Object.LastName.Should().BeEquivalentTo("DAVES");
 
         }
 
         [Fact]
         public void CheckFullNameContains()
         {
-            _sut.FirstName = "Timmy";
-            _sut.LastName = "Oleson";
-         //   Assert.Contains("mmy Ole", _sut.FullName);
-            _sut.FullName.Should().Contain("mmy Ole");
+           // _sut.FirstName = "Timmy";
+          //  _sut.LastName = "Oleson";
+
+            mock.Setup(pc => pc.FirstName).Returns("Timmy");
+            mock.Setup(pc => pc.LastName).Returns("Oleson");
+            mock.Setup(pc => pc.FullName).Returns(string.Concat(mock.Object.FirstName, " ", mock.Object.LastName));
+
+            //   Assert.Contains("mmy Ole", _sut.FullName);
+            mock.Object.FullName.Should().Contain("mmy Ole");
         }
         [Fact]
         //[Fact(Skip = "Don't need to run now")]
         public void CheckFullNameMatchesTitleCase()
         {
-            _sut.FirstName = "Timmy";
-            _sut.LastName = "Oleson";
+           // _sut.FirstName = "Timmy";
+           // _sut.LastName = "Oleson";
+
+            mock.Setup(pc => pc.FirstName).Returns("Tim");
+            mock.Setup(pc => pc.LastName).Returns("Oleson");
+            mock.Setup(pc => pc.FullName).Returns(string.Concat(mock.Object.FirstName, " ", mock.Object.LastName));
+
             //Assert.Matches("[A-Z]{1}[a-z]+ [A-z]{1}[a-z]+", _sut.FullName);
-            _sut.FullName.Should().MatchRegex("[A-Z]{1}[a-z]+ [A-z]{1}[a-z]+");
+            mock.Object.FullName.Should().MatchRegex("[A-Z]{1}[a-z]+ [A-z]{1}[a-z]+");
         }
 
         [Fact]
         public void StartWithDefaultHealth()
         {
             //Assert.Equal(100, _sut.Health);
-            _sut.Health.Should().Be(100);
+            mock.Setup(pc => pc.Health).Returns(100);
+            mock.Object.Health.Should().Be(100);
         }
 
         [Fact]
@@ -95,6 +122,7 @@ namespace GameEngine.Test
         {
             //Assert.NotEqual(0, _sut.Health);
             //To help understand the test when can provide optional becuase as in delow
+            mock.Setup(pc => pc.Health).Returns(100);
             _sut.Health.Should().NotBe(0, because: "Starting health should be 100");
 
         }
@@ -103,6 +131,9 @@ namespace GameEngine.Test
         public void IncreasedHealthAfterSleep()
         {
             _sut.Sleep();
+
+           
+
             //Assert.True(_sut.Health >=101 && _sut.Health <= 200);
             _sut.Health.Should().BeGreaterThan(101).And.BeLessThan(201);
         }
@@ -110,8 +141,9 @@ namespace GameEngine.Test
         [Fact]
         public void IncreasedHealthAfterSleepUsingRange()
         {
-            _sut.Sleep();
-           // Assert.InRange<int>(_sut.Health, 101 , 200);
+              _sut.Sleep();
+
+                       // Assert.InRange<int>(_sut.Health, 101 , 200);
             _sut.Health.Should().BeInRange(101, 201);
         }
 
@@ -125,8 +157,8 @@ namespace GameEngine.Test
         [Fact]
         public void HaveALongBow()
         {
-          
-           var listOfWeapons = _sut.Weapons;
+
+            var listOfWeapons = _sut.Weapons;
 
 
             // Assert.Contains("Long Dow", _sut.Weapons);
@@ -137,7 +169,7 @@ namespace GameEngine.Test
         [Fact]
         public void NotHaveAStaffOfWonder()
         {
-          //  Assert.DoesNotContain("Staff of Wonder", _sut.Weapons);
+            //  Assert.DoesNotContain("Staff of Wonder", _sut.Weapons);
             _sut.Weapons.Should().NotContain("Staff of Wonder");
         }
 
@@ -152,8 +184,10 @@ namespace GameEngine.Test
         [Fact]
         public void HaveNoEmptyDefaultWeapon()
         {
-            Assert.All(_sut.Weapons, weapon => Assert.False(string.IsNullOrWhiteSpace(weapon)));
+            //Assert.All(_sut.Weapons, weapon => Assert.False(string.IsNullOrWhiteSpace(weapon)));
             _sut.Weapons.Should().NotContainNulls();
+
+
         }
 
         [Fact]
@@ -170,14 +204,14 @@ namespace GameEngine.Test
                 _sut.Sleep();
                 monitoredSubject.Should().Raise("PlayerSlept");
             }
-            
+
         }
 
         [Fact]
         public void ExameString()
         {
             string actual = "THISISASTRING";
-            actual.Should().StartWith("TH").And.EndWith("NG").And.Contain("AS").And.HaveLength(12);
+            actual.Should().StartWith("TH").And.EndWith("NG").And.Contain("AS").And.HaveLength(13);
         }
 
 
@@ -185,7 +219,7 @@ namespace GameEngine.Test
         [Trait("Catogory", "HealthTest")]
         public void RaisePropertyChangedEvent()
         {
-           // Assert.PropertyChanged(_sut, "Health", () => _sut.TakeDamage(10));
+            // Assert.PropertyChanged(_sut, "Health", () => _sut.TakeDamage(10));
 
             using (var monitoredSubject = _sut.Monitor())
             {
@@ -195,7 +229,7 @@ namespace GameEngine.Test
                 //long form 
                 //monitoredSubject.Should().Raise("PropertyChanged").WithSender(_sut).WithArgs<PropertyChangedEventArgs>(args => args.PropertyName == "Health");
             }
-            
+
         }
 
         [Theory]
---
+++ GameEngine/IPlayerCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace GameEngine
+{
+    public interface IPlayerCharacter
+    {
+        string FirstName { get; set; }
+        string FullName { get; }
+        int Health { get; set; }
+        bool IsNoob { get; set; }
+        string LastName { get; set; }
+        string Nickname { get; set; }
+        List<string> Weapons { get; set; }
+
+        event EventHandler<EventArgs> PlayerSlept;
+        event PropertyChangedEventHandler PropertyChanged;
+
+        void Sleep();
+        void TakeDamage(int damage);
+    }
+}
